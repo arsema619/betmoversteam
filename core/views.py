1612d5e0947ad6e8a903814718eb01b7b7276b7c
Arsema <arsemamenberu16@gmail.com>
2026-06-12 08:05:21 -0700

    fix login view and routing

--- core/views.py
+++ core/views.py
@@ -1007,25 +1007,7 @@ def home(request):
 
 
 
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
 
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect("/")  # or dashboard
-
-        return render(request, "home.html", {
-            "error": "Invalid username or password"
-        })
-
-    return render(request, "home.html")
 
 
 def employee_business_rule(request):
@@ -1039,6 +1021,27 @@ def employee_business_rule(request):
             'rule': rule
         }
     )
+
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')   # login success
+        else:
+            return render(request, "login.html", {
+                "error": "Invalid username or password"
+            })
+
+    return render(request, "login.html")
 
 @login_required
 def total_progress(request):
