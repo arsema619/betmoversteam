569b5c286f3626bef2a8ad6e84b02eb53de843da
Arsema <arsemamenberu16@gmail.com>
2026-06-12 06:55:51 -0700

    Fix login form

--- core/views.py
+++ core/views.py
@@ -1005,8 +1005,25 @@ def home(request):
 
     return render(request, "home.html")
 
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
 def login_view(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')   # login success
+        else:
+            return render(request, "login.html", {
+                "error": "Invalid username or password"
+            })
+
+    return render(request, "login.html")
 
 def employee_business_rule(request):
 
