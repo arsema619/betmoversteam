8e7f577c59c18a9c982b5d396c9f6ddac66949bf
Arsema <arsemamenberu16@gmail.com>
2026-06-12 04:47:02 -0700

    fix production database + migrations

--- betmovers/settings.py
+++ betmovers/settings.py
@@ -82,12 +82,15 @@ WSGI_APPLICATION = 'betmovers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        default=os.environ.get(
+            'DATABASE_URL',
+            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        )
     )
-}   
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
