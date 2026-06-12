9de0eec7fe4013e72d06680374c32a8f298f143a
Arsema <arsemamenberu16@gmail.com>
2026-06-12 04:40:26 -0700

    fix database config

--- betmovers/settings.py
+++ betmovers/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'betmovers.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
-}
+}   
 
 
 # Password validation
