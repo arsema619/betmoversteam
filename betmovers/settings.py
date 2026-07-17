51256dd2d4d8c01e7cac29db89216e357b0f96ee
Arsema <arsemamenberu16@gmail.com>
2026-07-16 21:47:37 -0700

    Switch to Supabase database

--- betmovers/settings.py
+++ betmovers/settings.py
@@ -88,7 +88,9 @@ DATABASES = {
         default=os.environ.get(
             'DATABASE_URL',
             f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-        )
+        ),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
